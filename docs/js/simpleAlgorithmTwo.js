27c5a46a70b4c61c0dcb634d8178a48e8e4e5ec2
BlackUncle666 <BlackUncle666@163.com>
2022-01-17 15:06:13 +0800

    update:旋转

--- docs/js/simpleAlgorithmTwo.js
+++ docs/js/simpleAlgorithmTwo.js
@@ -133,3 +133,23 @@ const permute = (nums) => {
     dfs(nums, path);
     return res;
 };
+
+/**
+ * 48. 旋转图像
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
+ */
+const rotate = (matrix) => {
+    const n = matrix.length;
+    const matrix_new = new Array(n).fill(0).map(() => new Array(n).fill(0));
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            matrix_new[j][n - i - 1] = matrix[i][j];
+        }
+    }
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            matrix[i][j] = matrix_new[i][j];
+        }
+    }
+};
