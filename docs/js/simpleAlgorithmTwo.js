a60a37c59aa418afb3718ab0a744296667add129
BlackUncle666 <BlackUncle666@163.com>
2022-01-17 11:53:57 +0800

    update:接水

--- docs/js/simpleAlgorithmTwo.js
+++ docs/js/simpleAlgorithmTwo.js
@@ -77,3 +77,25 @@ const combinationSum2 = (candidates, target) => {
     backTracing(0, [], 0)
     return ans
 };
+
+
+/**
+ * 42. 接雨水
+ * @param {number[]} height
+ * @return {number}
+ */
+ const trap = height => {
+    let count = 0;
+    let [left, right] = [0, height.length - 1];
+    let [leftMax, rightMax] = [0, 0];
+    while (left < right) {
+        leftMax = Math.max(leftMax, height[left]);
+        rightMax = Math.max(rightMax, height[right]);
+        if (leftMax < rightMax) {
+            count += leftMax - height[left++];
+        } else {
+            count += rightMax - height[right--];
+        }
+    }
+    return count;
+};
