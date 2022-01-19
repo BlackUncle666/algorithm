61e873fdd0d0ebbda4a97914907280e825e7fd3a
BlackUncle666 <BlackUncle666@163.com>
2022-01-19 15:12:21 +0800

    udpate:路径

--- docs/js/simpleAlgorithmThree.js
+++ docs/js/simpleAlgorithmThree.js
@@ -119,3 +119,27 @@ const uniquePaths = (m, n) => {
     };
     return dp(m - 1, n - 1);
 };
+
+
+/**
+ * 63. 不同路径 II
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+const uniquePathsWithObstacles = (obstacleGrid) => {
+    const m = obstacleGrid.length
+    const n = obstacleGrid[0].length
+    const dp = Array(m).fill().map(item => Array(n).fill(0))
+    for (let i = 0; i < m && obstacleGrid[i][0] === 0; ++i) {
+        dp[i][0] = 1
+    }
+    for (let i = 0; i < n && obstacleGrid[0][i] === 0; ++i) {
+        dp[0][i] = 1
+    }
+    for (let i = 1; i < m; ++i) {
+        for (let j = 1; j < n; ++j) {
+            dp[i][j] = obstacleGrid[i][j] === 1 ? 0 : dp[i - 1][j] + dp[i][j - 1]
+        }
+    }
+    return dp[m - 1][n - 1]
+};
